98ba9fdf1311217b7b053085cd21072c7a35d0c7
chad steele <zippyskippy@gmail.com>
2017-11-16 22:53:45 -0700

    Merged with wordpress

--- header.php
+++ header.php
@@ -1,75 +1,1 @@
-<?php
-/**
- * The template for displaying the header
- *
- * Displays all of the head element and everything up until the "site-content" div.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/creative.min.css" type="text/css" media="screen" />
-
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css" type="text/css" media="screen" />
-
-	<!-- fonts -->
-	<link href="<?php echo get_stylesheet_directory_uri(); ?>/genericons/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,500i,700,700i|Raleway:400,400i,600,600i,800,800i,900,900i" rel="stylesheet">
-
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/blog-style.css" type="text/css" media="screen" />
-
-  
-    
-
-
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page-top">
-
-	<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png"></a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-       
-            <?php wp_nav_menu( array( 'theme_location' => 'main-nav',
-			'menu_class' => 'menu navbar-nav ml-auto'  ) ); ?>
-    
-
-        </div>
-      </div>
-    </nav>
-</div>
-<!-- .sidebar -->
-
-   <!-- <header id="headerInner">  /* Suppress the search bar in the Header - by JHazelwood 11Oct2017 */
-    <div class="Explore_input">
-
-      <form method="get" id="searchform" class="search-form" action="<?php echo get_permalink(WAZEE_SITE_TEMPLATE_SEARCH_RESULTS); ?>">
-          <label for="s" class="assistive-text">
-          <span class="screen-reader-text"><?php _e( 'Search', 'twentyfifteen' ); ?></span>
-          <input type="search" class="field search-field" name="q" id="s" placeholder="<?php esc_attr_e( 'Explore TRIARC now…', 'twentyfifteen' ); ?>" />
-          </label>
-
-          <input type="submit" class="submit search-submit screen-reader-text" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyfifteen' ); ?>" />
-      </form>
-
-     
-    </div>
-	</header> -->
-
+<?php/** * The template for displaying the header * * Displays all of the head element and everything up until the "site-content" div. * * @package WordPress * @subpackage Twenty_Fifteen * @since Twenty Fifteen 1.0 */?><!DOCTYPE html><html <?php language_attributes(); ?> class="no-js"><head>	<meta charset="<?php bloginfo( 'charset' ); ?>">	<meta name="viewport" content="width=device-width">	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/creative.min.css" type="text/css" media="screen" />	<link rel="profile" href="http://gmpg.org/xfn/11">	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">    <!-- fonts -->    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,500i,700,700i|Raleway:400,400i,600,600i,800,800i,900,900i"          rel="stylesheet">  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/blog-style.css" type="text/css" media="screen" />      	<!--[if lt IE 9]>	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>	<![endif]-->	<?php wp_head(); ?></head><body <?php body_class(); ?>><div id="page-top">    <nav class="navbar navbar-expand-lg fixed-top main-menu">        <div class="brand-container">            <a class="navbar-brand" href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png"></a>        </div>        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"                aria-expanded="false" aria-label="Toggle navigation">            <span class="fa fa-bars"></span>        </button>        <div class="collapse navbar-collapse" id="navbarSupportedContent">            <ul class="navbar-nav mr-auto">                <li class="nav-item dropdown">                    <a class="nav-link dropdown-toggle" href="#" id="CountriesDropDown" role="button"                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">                        Countries                    </a>                    <div class="dropdown-menu" aria-labelledby="CountriesDropDown">                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/abu-dhabi/">Abu Dhabi</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/angola/">Angola</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/bahrain/">Bahrain</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/dubai/">Dubai</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/egypt/">Egypt</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/europe/">Europe</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/india/">India</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/iran/">Iran</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/iraq/">Iraq</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/jordan/">Jordan</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/kuwait/">Kuwait</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/lebanon/">Lebanon</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/morocco/">Morocco</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/palestine/">Palestine</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/qatar/">Qatar</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/saudi-arabia/">Saudi Arabia</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/syria/">Syria</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/thailand/">Thailand</a>                       <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/turkey/">Turkey</a>                    </div>                </li>                <li class="nav-item dropdown">                    <a class="nav-link dropdown-toggle" href="#" id="CollectionsDropDown" role="button"                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">                        Collections                    </a>                    <div class="dropdown-menu" aria-labelledby="CollectionsDropDown">                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/modern-arab/">Modern Arab</a>                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/101-arabian-nights-and-days/">101 Arabian Nights… and Days</a>                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/super-structures-babylon-to-dubai/">Super Structures: Babylon to Dubai</a>                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/islamic-design/">Islamic Design</a>                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/the-cedars-of-god/">The Cedars of God</a>                        <a class="dropdown-item" href="http://demo.rockriverdev3.com/collections/treasure-trove/">Treasure Trove</a>                    </div>                </li>                <li class="nav-item">                    <a class="nav-link" href="/contact/">Contact</a>                </li>            </ul>        </div>    </nav>    <div class="search-row">        <form class="form-inline" action="/search-results/">            <label class="sr-only" for="navSearch">Search</label>            <input type="text" class="form-control" name="q" id="navSearch" placeholder="Explore TRIARC now...">            <button type="submit" class="btn btn-search"><span class="fa fa-search fa-lg"></span></button>        </form>    </div></div><!-- .sidebar -->   <!-- <header id="headerInner">  /* Suppress the search bar in the Header - by JHazelwood 11Oct2017 */    <div class="Explore_input">      <form method="get" id="searchform" class="search-form" action="<?php echo get_permalink(WAZEE_SITE_TEMPLATE_SEARCH_RESULTS); ?>">          <label for="s" class="assistive-text">          <span class="screen-reader-text"><?php _e( 'Search', 'twentyfifteen' ); ?></span>          <input type="search" class="field search-field" name="q" id="s" placeholder="<?php esc_attr_e( 'Explore TRIARC now…', 'twentyfifteen' ); ?>" />          </label>          <input type="submit" class="submit search-submit screen-reader-text" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyfifteen' ); ?>" />      </form>         </div>	</header> -->
